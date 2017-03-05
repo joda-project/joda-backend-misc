0938ac3bc3f8f1dc48d392d16a37e224489a124a
Tadej Novak <tadej@tano.si>
2017-03-05 13:31:47 +0100

    Create a default misc type that is not deletable (closes #1)

--- joda_misc/admin.py
+++ joda_misc/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from joda_core.documents.admin import DocumentsAdmin
 from joda_misc.models import MiscDocument, MiscType
@@ -11,6 +11,19 @@ class MiscDocumentsAdmin(DocumentsAdmin):
 
 class MiscTypesAdmin(admin.ModelAdmin):
     ordering = ('name',)
+
+    def has_delete_permission(self, request, obj=None):
+        # If we're running the bulk delete action, estimate the number
+        # of objects after we delete the selected items
+        selected = request.POST.getlist(admin.helpers.ACTION_CHECKBOX_NAME)
+        print(selected)
+        if (selected and str(MiscType.DEFAULT_PK) in selected) \
+                or (obj and obj.pk == MiscType.DEFAULT_PK):
+            message = 'The first (default) type can not be deleted.'
+            self.message_user(request, message, messages.INFO)
+            return False
+
+        return super(MiscTypesAdmin, self).has_delete_permission(request, obj)
 
 
 admin.site.register(MiscDocument, MiscDocumentsAdmin)
